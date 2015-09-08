7f1ff143487a44d079cf5b4b80cde48373b79def
Alberto Otero de la Roza <alberto@fluor.quimica.uniovi.es>
2015-09-08 01:34:03 -0600

    random walk code updated for penalty function

--- dcp_rwalk.m
+++ dcp_rwalk.m
@@ -2,7 +2,7 @@
 
 format long
 global dcp basis db prefix nstep verbose run_inputs ycur dcpfin...
-       costmin iload stime0 astep dcpeval maxnorm fixnorm
+       costmin iload stime0 astep dcpeval maxnorm fixnorm muk dcp0
 
 #### Modify this to change the input behavior ####
 
@@ -32,47 +32,8 @@ mem=2;
 
 ## List of database files to use in DCP optimization
 listdb={...
-"atz_blyp/s225_2pyridoxine2aminopyridin09.db","atz_blyp/s225_2pyridoxine2aminopyridin10.db","atz_blyp/s225_2pyridoxine2aminopyridin12.db",...
-"atz_blyp/s225_2pyridoxine2aminopyridin15.db","atz_blyp/s225_2pyridoxine2aminopyridin20.db","atz_blyp/s225_adeninethyminestack09.db",...
-"atz_blyp/s225_adeninethyminestack10.db","atz_blyp/s225_adeninethyminestack12.db","atz_blyp/s225_adeninethyminestack15.db",...
-"atz_blyp/s225_adeninethyminestack20.db","atz_blyp/s225_adeninethymineWC09.db","atz_blyp/s225_adeninethymineWC10.db",...
-"atz_blyp/s225_adeninethymineWC12.db","atz_blyp/s225_adeninethymineWC15.db","atz_blyp/s225_adeninethymineWC20.db",...
-"atz_blyp/s225_ammoniadimer09.db","atz_blyp/s225_ammoniadimer10.db","atz_blyp/s225_ammoniadimer12.db",...
-"atz_blyp/s225_ammoniadimer15.db","atz_blyp/s225_ammoniadimer20.db","atz_blyp/s225_benzeneammonia09.db",...
-"atz_blyp/s225_benzeneammonia10.db","atz_blyp/s225_benzeneammonia12.db","atz_blyp/s225_benzeneammonia15.db",...
-"atz_blyp/s225_benzeneammonia20.db","atz_blyp/s225_benzenedimerstack09.db","atz_blyp/s225_benzenedimerstack10.db",...
-"atz_blyp/s225_benzenedimerstack12.db","atz_blyp/s225_benzenedimerstack15.db","atz_blyp/s225_benzenedimerstack20.db",...
-"atz_blyp/s225_benzenedimerTshape09.db","atz_blyp/s225_benzenedimerTshape10.db","atz_blyp/s225_benzenedimerTshape12.db",...
-"atz_blyp/s225_benzenedimerTshape15.db","atz_blyp/s225_benzenedimerTshape20.db","atz_blyp/s225_benzeneHCN09.db",...
-"atz_blyp/s225_benzeneHCN10.db","atz_blyp/s225_benzeneHCN12.db","atz_blyp/s225_benzeneHCN15.db",...
-"atz_blyp/s225_benzeneHCN20.db","atz_blyp/s225_benzenemethane09.db","atz_blyp/s225_benzenemethane10.db",...
-"atz_blyp/s225_benzenemethane12.db","atz_blyp/s225_benzenemethane15.db","atz_blyp/s225_benzenemethane20.db",...
-"atz_blyp/s225_benzenewater09.db","atz_blyp/s225_benzenewater10.db","atz_blyp/s225_benzenewater12.db",...
-"atz_blyp/s225_benzenewater15.db","atz_blyp/s225_benzenewater20.db","atz_blyp/s225_ethenedimer09.db",...
-"atz_blyp/s225_ethenedimer10.db","atz_blyp/s225_ethenedimer12.db","atz_blyp/s225_ethenedimer15.db",...
-"atz_blyp/s225_ethenedimer20.db","atz_blyp/s225_etheneethyne09.db","atz_blyp/s225_etheneethyne10.db",...
-"atz_blyp/s225_etheneethyne12.db","atz_blyp/s225_etheneethyne15.db","atz_blyp/s225_etheneethyne20.db",...
-"atz_blyp/s225_formamidedimer09.db","atz_blyp/s225_formamidedimer10.db","atz_blyp/s225_formamidedimer12.db",...
-"atz_blyp/s225_formamidedimer15.db","atz_blyp/s225_formamidedimer20.db","atz_blyp/s225_formicaciddimer09.db",...
-"atz_blyp/s225_formicaciddimer10.db","atz_blyp/s225_formicaciddimer12.db","atz_blyp/s225_formicaciddimer15.db",...
-"atz_blyp/s225_formicaciddimer20.db","atz_blyp/s225_indolebenzenestack09.db","atz_blyp/s225_indolebenzenestack10.db",...
-"atz_blyp/s225_indolebenzenestack12.db","atz_blyp/s225_indolebenzenestack15.db","atz_blyp/s225_indolebenzenestack20.db",...
-"atz_blyp/s225_indolebenzeneTshape09.db","atz_blyp/s225_indolebenzeneTshape10.db","atz_blyp/s225_indolebenzeneTshape12.db",...
-"atz_blyp/s225_indolebenzeneTshape15.db","atz_blyp/s225_indolebenzeneTshape20.db","atz_blyp/s225_methanedimer09.db",...
-"atz_blyp/s225_methanedimer10.db","atz_blyp/s225_methanedimer12.db","atz_blyp/s225_methanedimer15.db",...
-"atz_blyp/s225_methanedimer20.db","atz_blyp/s225_phenoldimer09.db","atz_blyp/s225_phenoldimer10.db",...
-"atz_blyp/s225_phenoldimer12.db","atz_blyp/s225_phenoldimer15.db","atz_blyp/s225_phenoldimer20.db",...
-"atz_blyp/s225_pyrazinedimer09.db","atz_blyp/s225_pyrazinedimer10.db","atz_blyp/s225_pyrazinedimer12.db",...
-"atz_blyp/s225_pyrazinedimer15.db","atz_blyp/s225_pyrazinedimer20.db","atz_blyp/s225_uracildimerHB09.db",...
-"atz_blyp/s225_uracildimerHB10.db","atz_blyp/s225_uracildimerHB12.db","atz_blyp/s225_uracildimerHB15.db",...
-"atz_blyp/s225_uracildimerHB20.db","atz_blyp/s225_uracildimerstack09.db","atz_blyp/s225_uracildimerstack10.db",...
-"atz_blyp/s225_uracildimerstack12.db","atz_blyp/s225_uracildimerstack15.db","atz_blyp/s225_uracildimerstack20.db",...
-"atz_blyp/s225_waterdimer09.db","atz_blyp/s225_waterdimer10.db","atz_blyp/s225_waterdimer12.db",...
-"atz_blyp/s225_waterdimer15.db","atz_blyp/s225_waterdimer20.db",...
 };
-weightdb=[1 5 1 1 1 1 5 1 1 1 1 5 1 1 1 1 5 1 1 1 1 5 1 1 1 1 5 1 1 1 1 5 1 1 1 1 5 1 1 1 1 5 1 1 1 ...
-          1 5 1 1 1 1 5 1 1 1 1 5 1 1 1 1 5 1 1 1 1 5 1 1 1 1 5 1 1 1 1 5 1 1 1 1 5 1 1 1 1 5 1 1 1 ...
-          1 5 1 1 1 1 5 1 1 1 1 5 1 1 1 1 5 1 1 1];
+weightdb=[];
 
 ## Initial DCP file (you can use a cell array of files here, like
 ## {"C.dcp","H.dcp"}, or a single string "bleh.dcp")
@@ -82,6 +43,10 @@ dcpini="dcp.ini";
 ## the script is running, dcpfin contains the DCP for the evaluation
 ## with lowest cost function.
 dcpfin="dcp.fin";
+
+## This DCP will be included in all the calculations but it will not 
+## be optimized. 
+dcpfix="dcp.fix";
 
 ## Final evaluation file. Contains the evaluation of the best DCP
 ## found on hte parametrization set. While the script is running,
@@ -102,14 +67,14 @@ funeval   = "fbasic";
 funevald2 = "fbasicd2";
 
 ## Name of the Gaussian input runner routine
-## run_inputs = @run_inputs_serial; ## Run all Gaussian inputs sequentially on the same node
+run_inputs = @run_inputs_serial; ## Run all Gaussian inputs sequentially on the same node
 ## run_inputs = @run_inputs_grex; ## Submit inputs to the queue, wait for all to finish. Grex version.
 ## run_inputs = @run_inputs_plonk; ## Submit inputs to a private queue, plonk version.
-run_inputs = @run_inputs_nint_trasgu; ## Submit inputs to a private queue on the NINT cluster.
+## run_inputs = @run_inputs_nint_trasgu; ## Submit inputs to a private queue on the NINT cluster.
 ## run_inputs = @run_inputs_elcap3; ## Submit inputs to elcap3.
 
 ## Tolerance criteria for the minimization (function difference between successive steps)
-ftol = 1d-1; ## function change tolerance for random walk batches
+ftol = 1d-2; ## function change tolerance for random walk batches
 
 ## Number of random batches
 nrnd = Inf;
@@ -124,13 +89,14 @@ rndstep = 1d-3;
 
 ## Norm constraint: the norm of the coefficients is constrained to
 ## this value. (optional)
-fixnorm = 1d-2;
+## fixnorm = 1d-3;
+## normtol = 1d-2;
 
 #### No touching past this point. ####
 
 ## Header
 printf("### Random walk started on %s ###\n",strtrim(ctime(time())));
-printf("# PID: %d \n",getpid());
+printf("### PID: %d ###\n",getpid());
 
 ## Read the basis set
 basis = parsebasis(basis);
@@ -140,6 +106,21 @@ dcp = parsedcp(dcpini);
 if (verbose) 
   printf("### Initial DCP ###\n");
   writedcp(dcp);
+endif
+
+## Read the fixed DCP
+if (exist("dcpfix","var") && !isempty(dcpfix))
+  dcp0 = parsedcp(dcpfix);
+  ## Check that the atoms in dcp and dcp0 do not clash
+  for i = 1:length(dcp)
+    for j = 1:length(dcp0)
+      if (strcmp(tolower(dcp{i}.atom),tolower(dcp0{j}.atom)))
+        error("Clashing atom in dcpini and dcpfix")
+      endif
+    endfor
+  endfor
+else
+  dcp0 = {};
 endif
 
 ## Read the parametrization database 
@@ -178,10 +159,12 @@ costmin = Inf;
 iload = [];
 stime0 = time();
 x = packdcp(dcp);
-if (exist("fixnorm","var") && fixnorm > 0)
-  x(end) = sqrt(fixnorm^2 - sum(x(2:2:end).^2));
-endif
 n = length(x) / 2;
+muk = 0;
+
+## Save the initial random seed
+v = rand("state");
+save "dcp_rwalk.seed" v;
 
 ## Evaluate the zero DCP 
 printf("# Evaluating the zero DCP cost\n");
@@ -189,9 +172,19 @@ x0 = x;
 x0(2:2:end) = 0;
 cost0 = feval(funeval,x0);
 
-## Save the initial random seed
-v = rand("state");
-save "dcp_rwalk.seed" v;
+## Set initial penalty coefficient
+if (exist("fixnorm","var") && fixnorm > 0)
+  ssq = sum(x(2:2:end).^2);
+  px = ssq/fixnorm^2 - 1;
+  printf("### Initial cost: %.10f\n",cost0);
+  printf("### Initial norm: %.10f\n",sqrt(ssq));
+  printf("### Target norm: %.10f\n",fixnorm);
+  if (abs(px) > 1d-3)
+    muk0 = 0.01 * cost0 / px^2;
+  else
+    muk0 = cost0;
+  endif
+endif
 
 irnd = 1;
 while (irnd <= nrnd)
@@ -256,8 +249,41 @@ while (irnd <= nrnd)
   ## Accept the step and launch the minimization 
   if (iaccept)
     irnd++;
+    x = xnew;
     cost0 = cost;
-    [xmin, ymin] = d2_min(funeval,funevald2,xnew,ftol);
+
+    ## Run the minimization
+    nopt = 0;
+    muk = 0;
+    while true
+      costmin = Inf;
+      nopt = nopt + 1;
+      if (exist("fixnorm","var") && fixnorm > 0)
+        printf("### Minimization number %d\n",nopt);
+        printf("### Penalty mu: %.4e\n",muk);
+      endif
+      ## Minimize
+      [xmin, ymin] = d2_min(funeval,funevald2,x,ftol);
+
+      if (exist("fixnorm","var") && fixnorm > 0)
+        x = xmin;
+        if (nopt == 1)
+          muk = muk0 / 10;
+        endif
+        muk = muk * 10;
+        cn = sqrt(sum(x(2:2:end).^2));
+        printf("### Minimization done\n");
+        printf("### Coefficient norm: %.10f\n",cn);
+        printf("### Target norm: %.10f\n",fixnorm);
+        if (abs(cn - fixnorm)/fixnorm <= normtol)
+          break
+        else
+          printf("### Penalty mu: %.4e\n",muk);
+        endif
+      else
+        break
+      endif
+    endwhile
     x = xmin;
   endif
 endwhile
